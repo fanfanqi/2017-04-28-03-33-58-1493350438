f11c1c425ae1255ff26136f09af4eef54b7c93c2
fanfanqi <fanrong_me@163.com>
2017-05-10 21:17:02 +0800

    modify second time

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,61 +1,68 @@
 import java.util.HashMap;
 public class BowlingGame {
 
-    public int getBowlingScore(String bowlingCode) {
-    	int score=0;
-    	String[] arr = bowlingCode.split("|");//分割字符串
-    	if(arr.length==12)
-    		arr[10]=arr[11];
-    	for(int i=0;i<arr.length;i++)//遍历十个格子
-    	System.out.println(arr[i]);
-    	//计算十个格子的分数
-		for(int i=0;i<10;i++){
-			String tmp=arr[i];//第i 个格子的标记
-			score+=getScore(tmp,i,arr);
-		}
+    public static int getBowlingScore(String res) {
+        int score = 0;
+
+        //将字符串分割
+        String[] source = res.split("\\|");
+        if(source.length == 12)
+            source[10] = source[11];
+
+        //对十个格子分数进行计算
+        for(int i = 0; i < 10; i++){
+            String tmp = source[i];//获得第i个格子的标记
+            score += calScore(tmp, i, source);
+        }
         return score;
     }
-    public int getScore(String flag,int i,String[] arr){
-    	HashMap map = new HashMap<Character,Integer>();
-    	map.put('1',1);
-    	map.put('2',2);
-    	map.put('3',3);
-    	map.put('4',4);
-    	map.put('5',5);
-    	map.put('6',6);
-    	map.put('7',7);
-    	map.put('8',8);
-    	map.put('9',9);
-    	map.put('X',10);
-    	map.put('-',0);
-    	map.put('/',10);
-    	//获取flag的长度
-    	int size = flag.length();
-    	int sco = 0;
-    	//size==1 表示X，strike
-    	if(size==1){
-    		if(arr[i+1].length()==2){//下一格有两个记录
-    			if(arr[i+1].charAt(1)!='/'){//下一个记录不含有'/'
-    			int first = (int)map.get(arr[i+1].charAt(0));
-    			int second = (int)map.get(arr[i+1].charAt(1))+first;
-    			sco = 10+(first>second?first:second);//sco=10+second;
-    			}else{//下一个记录含有'/'
-    			sco=10+10;
-    			}
-    		}else{//下一格只有一个记录(有一个strike)，还需加上在下一个的第一个记录
-    			sco=10+10+(int)map.get(arr[i+2].charAt(0));
-    		}
-    	}
-    	if(size==2){//有两种情况，1有spare,2无spare
-    		if(flag.charAt(1)=='/'){//1有spare
-    			sco=10+(int)map.get(arr[i+1].charAt(0));
-    		}else{//2无spare
-    			int first =(int)map.get(arr[i].charAt(0));
-    			int second =(int)map.get(arr[i].charAt(1))+first;
-    			sco = first>second?first:second;//sco=second;
-    		}
-    	}
-    	return sco;
+
+    public static int calScore(String flag, int i, String[] source) {
+        HashMap map = new HashMap<Character, Integer>();
+        map.put('1', 1);
+        map.put('2', 2);
+        map.put('3', 3);
+        map.put('4', 4);
+        map.put('5', 5);
+        map.put('6', 6);
+        map.put('7', 7);
+        map.put('8', 8);
+        map.put('9', 9);
+        map.put('X', 10);
+        map.put('-', 0);
+        map.put('/', 10);
+        //获得flag的长度
+        int size = flag.length();
+        int sco = 0;
+        //size为1,则一定是X,strike
+        if(size == 1) {
+            if(source[i+1].length() == 2) {//下一格有两个记录
+                if(source[i+1].charAt(1) != '/') {//下一个记录中不含有'/'
+                    int first = (int) map.get(source[i + 1].charAt(0));
+                    int second = (int) map.get(source[i + 1].charAt(1)) + first;
+                    sco = 10 + (first > second ? first : second);
+                }
+                else {//下一格记录中含有'/'
+                    sco = 10 + 10;
+                }
+            }
+            else {// 下一个只要一个记录,则得再加上再下一格的记录
+                //sco = 10 + (int)map.get(source[i+1].charAt(0)) + (int) map.get(source[i+2].charAt(0));
+                sco = 10 + 10 + (int) map.get(source[i+2].charAt(0));
+            }
+        }
+        //size为2,则有两种情况,第一就是有spare,第二就是没有spare
+        if(size == 2) {//  4/|3/|2/|...的情况
+            if(flag.charAt(1) == '/') {//spare
+                sco = 10 + (int) map.get(source[i+1].charAt(0));
+            }
+            else {//非spare
+                int first = (int) map.get(source[i].charAt(0));
+                int second = (int) map.get(source[i].charAt(1)) + first;
+                sco = first > second ? first : second;
+            }
+        }
+        return sco;
     }
 
 }
